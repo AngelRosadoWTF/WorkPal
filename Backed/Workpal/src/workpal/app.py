a9f4dbc7c689997b7b77eaae3c163233f53ba263
Pige <85260656+GPigE@users.noreply.github.com>
2026-03-19 20:51:08 -0600

    ahora si conectar el router de proyectos

--- Backed/Workpal/src/workpal/app.py
+++ Backed/Workpal/src/workpal/app.py
@@ -2,6 +2,7 @@
 from Backed.Workpal.Database.Models import Alumno, Proyecto, ProyectoAlumno
 from Backed.Workpal.Database.conexion import engine, SessionLocal
 from .router.Alumnos import router as alumnos_router
+from .router.Proyectos import router as proyectos_router
 from fastapi import FastAPI
 # Registrar todos los modelos antes de crear las tablas
 from Backed.Workpal.Database.Models.Alumnos import Alumno
@@ -14,3 +15,4 @@ app = FastAPI()
 Base.metadata.create_all(engine)
 
 app.include_router(alumnos_router)
+app.include_router(proyectos_router)
