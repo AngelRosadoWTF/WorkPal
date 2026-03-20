d20eb644ca735facc217f71bdd5e03fb9ce6a409
Pige <85260656+GPigE@users.noreply.github.com>
2026-03-19 20:33:13 -0600

    conectar router proyectos

--- Backed/Workpal/src/workpal/__init__.py
+++ Backed/Workpal/src/workpal/__init__.py
@@ -1,6 +1,8 @@
 #todos los router
 from .router.Alumnos import router as alumnos_router
+from .router.Proyectos import router as proyectos_router
 
 __all__=(
     "alumnos_router",
+    "proyectos_router"
 )
