a3fe6fc206f525684f42a0192bbc4ba34ca27e60
Pige <85260656+GPigE@users.noreply.github.com>
2026-05-12 18:54:46 -0600

    arreglar conexion

--- Backed/Workpal/Database/conexion.py
+++ Backed/Workpal/Database/conexion.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from .Models.base import Base
 
-# Cargar variables de entorno
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./.blog.db")
@@ -15,8 +14,19 @@ if DATABASE_URL.startswith("sqlite"):
     engine_kwargs["connect_args"] = {"check_same_thread": False}
 
 engine = create_engine(DATABASE_URL, echo=True, future=True, **engine_kwargs)
+
 SessionLocal = sessionmaker(
-    bind=engine, autoflush=False, autocommit=False, class_=Session
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    class_=Session
 )
 
-Base.metadata.create_all(bind=engine)  # dev
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
